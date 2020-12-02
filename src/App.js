418dbd7e647578d365c894ad1dcdf25de39a5265
evangel <evangeldaping@gmail.com>
2020-12-01 22:33:49 -0800

    Content and Sidebar Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Header } from './components/Header'
+import { Content } from './components/Content'
 
 export const App = () => (
   <div className="App">
     <Header />
+    <Content />
   </div>
 );
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar } from 'react-icons/fa'
+
+export const Sidebar = () => {
+    return (
+        <div className="sidebar" data-testid="sidebar">
+            <ul className="sidebar__generic">
+                <li><span><FaInbox/></span><span>Inbox</span></li>
+                <li><FaRegCalendar/>Today</li>
+                <li><FaRegCalendarAlt/>Next 7 days</li>
+            </ul>
+
+            <div className="sidebar__middle">
+                <span><FaChevronDown/></span>
+                <h2>Projects</h2>
+            </div>
+
+            <ul className="sidebar__projects">Projects will be here!</ul>
+            Add Projects Component here!!
+        </div>
+    )
+};
