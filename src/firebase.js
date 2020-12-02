14addafe29030832ec6769b3e108719aad9e66f9
evangel <evangeldaping@gmail.com>
2020-12-01 23:21:03 -0800

    Adding Firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = firebase.initialize({
+    apiKey: "AIzaSyBEN5PSGgVT06U2wPdVHhMwPYchA3brUho",
+    authDomain: "todoist-5b4f0.firebaseapp.com",
+    databaseURL: "https://todoist-5b4f0.firebaseio.com",
+    projectId: "todoist-5b4f0",
+    storageBucket: "todoist-5b4f0.appspot.com",
+    messagingSenderId: "307471461186",
+    appId: "1:307471461186:web:e662f26a44d1ea5db5f6db",
+});
+
+export { firebaseConfig as firebase };
