101c4f910ac4bfea9779076f1d565d07fec07565
evangel <evangeldaping@gmail.com>
2020-12-02 23:24:20 -0800

    Building the Projects Component

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar } from 'react-icons/fa'
-import { useSelectedProjectValue } from '../../context';
+import { Projects } from '../Projects';
 
 export const Sidebar = () => {
-    const { selectedProject } = useSelectedProjectValue;
+    // const { selectedProject } = useSelectedProjectValue;
     const [active, setActive] = useState('inbox');
     const [showProjects, setShowProjects] = useState(true);
 
@@ -20,8 +20,8 @@ export const Sidebar = () => {
                 <h2>Projects</h2>
             </div>
 
-            <ul className="sidebar__projects">Projects will be here!</ul>
-            Add Projects Component here!!
+            <ul className="sidebar__projects">{showProjects && <Projects/>}</ul>
+            {showProjects && <Projects/>}
         </div>
     )
 };
