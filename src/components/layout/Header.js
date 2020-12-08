b4312b3450c194c6437754322645bbe310faa41c
evangel <evangeldaping@gmail.com>
2020-12-07 23:57:51 -0800

    Adding to the Header component (dark mode implementation)

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,21 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { FaPizzaSlice } from 'react-icons/fa'
-export const Header = () => {
+
+export const Header = ({ darkMode, setDarkMode }) => {
+    const [shouldShowMain, setShouldShowMain] = useState(false);
+    const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+
     return (
         <header className="header" data-testid="header">
-            <nav>
-                <div className="logo">
-                    <img src="/images/logo.png" alt="Todoist"/>
-                </div>
-                <div className="settings">
-                    <ul>
-                        <li>+</li>
-                        <li>
-                            <FaPizzaSlice/>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+        <nav>
+            <div className="logo">
+            <img src="/images/logo.png" alt="Todoist" />
+            </div>
+            <div className="settings">
+            <ul>
+                <li className="settings__add">
+                <button
+                    data-testid="quick-add-task-action"
+                    aria-label="Quick add task"
+                    type="button"
+                    onClick={() => {
+                    setShowQuickAddTask(true);
+                    setShouldShowMain(true);
+                    }}
+                >
+                    +
+                </button>
+                </li>
+                <li className="settings__darkmode">
+                <button
+                    data-testid="dark-mode-action"
+                    aria-label="Darkmode on/off"
+                    type="button"
+                    onClick={() => setDarkMode(!darkMode)}
+                >
+                    <FaPizzaSlice />
+                </button>
+                </li>
+            </ul>
+            </div>
+        </nav>
+    </header>
     )
 }
